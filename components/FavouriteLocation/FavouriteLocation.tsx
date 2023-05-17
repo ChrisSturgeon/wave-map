dfb362d4e82e97446c5eb210248298d183903d46
csturgeon <sturgeon.chris@gmail.com>
2023-05-17 06:07:15 +0100

    Commit to add swr to favouritelocation component

--- components/FavouriteLocation/FavouriteLocation.tsx
+++ components/FavouriteLocation/FavouriteLocation.tsx
@@ -2,6 +2,13 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import style from './FavouriteLocation.module.css';
 
+async function fetcher(locationId: string) {
+  const url = `/api/location/favourite/${locationId}`;
+  const response = await fetch(url);
+  const data = response.json();
+  return data;
+}
+
 export default function FavouriteLocation() {
   const router = useRouter();
   const locationId = router.query['locationId'];
@@ -18,6 +25,8 @@ export default function FavouriteLocation() {
       } else {
         setHasFavourited(false);
       }
+
+      setIsActive(true);
     }
     checkIsFavourited();
   }, [locationId]);
@@ -51,29 +60,31 @@ export default function FavouriteLocation() {
 
   return (
     <div className={style.favourite}>
-      <p>
+      {/* <p>
         {hasFavourited ? 'You have' : 'You have not'} favourited this location!{' '}
         {locationId}
-      </p>
+      </p> */}
       <button onClick={toggleFavourite}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1}
-          stroke="currentColor"
-          className={hasFavourited ? style.filled : style.empty}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-          />
-        </svg>
+        {isActive && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1}
+            stroke="currentColor"
+            className={hasFavourited ? style.filled : style.empty}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+            />
+          </svg>
+        )}
       </button>
-      <button onClick={() => console.log(hasFavourited)}>
+      {/* <button onClick={() => console.log(hasFavourited)}>
         Check favourite status
-      </button>
+      </button> */}
     </div>
   );
 }
